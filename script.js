da97d294e72dd3f0c95221e1ec7fab21a9a4488b
Saivarshini-N <92864526+Saivarshini-N@users.noreply.github.com>
2023-06-06 20:05:36 +0530

    Updating js file

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert("Hello I'm Varshini");
+var data1={image : "picture.jpg",name : "Saivarshini", designation:"Student"};
+var data2={image : "picture2.jpg",name : "varshi", designation:"Student"};
+var isdata1 = true;
+var displaydata;
+var changemypic = function(){
+    if(isdata1){
+        displaydata= data2;
+        isdata1 = false;
+    }
+    else{
+        displaydata = data1;
+        isdata1=true;
+    }
+    document.getElementById("myimage").src=displaydata.image;
+    document.getElementById("myname").innerHTML=displaydata.name;
+    document.getElementById("mydesig").innerHTML=displaydata.designation;
+}
