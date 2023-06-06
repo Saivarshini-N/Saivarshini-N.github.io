da0615c44283aa1bf5ade56f008fdbeab293a8c5
Saivarshini-N <92864526+Saivarshini-N@users.noreply.github.com>
2023-06-06 22:20:06 +0530

    Updated user card

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-var data1 = {image:"./Current.jpg",name:"Saivarshini",designation:"Student"}
-var data2 = {image:"./childhood.jpg",name:"Varshi",designation:"SDE"}
+ar data1 = {image : "./childhood.jpg", name : "Varshi", designation : "Student"};
+var data2 = {image : "./Current.jpg", name : "Saivarshini", designation : "Student"};
 var isData1 = true;
 var displayData;
-var changeMyPic = function(){
-    if(isData1){
+var changeMyPic = function()
+{
+    if(isData1)
+    {
         displayData = data2;
         isData1 = false;
     }
-    else{
+    else
+    {
         displayData = data1;
         isData1 = true;
     }
-    document.getElementById("myImage").src = displayData.image;
-    document.getElementById("myName").innerHTML=displayData.name;
-    document.getElementById("myDsng").innerHTML=displayData.designation;
+    document.getElementById("myChildImg").src = displayData.image;
+    document.getElementById("myFirstName").innerHTML = displayData.name;
+    document.getElementById("myDesignation").innerHTML = displayData.designation;
 }
